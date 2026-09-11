5bc5c94f41cd20195038f56ff141dfccf4ae1f88
Jon-Fredrik Nielsen <jfnielsen@gmail.com>
2026-09-11 11:51:06 -0400

    On branch dev 	modified:   setup.m

--- scanner/compile/setup.m
+++ scanner/compile/setup.m
@@ -6,8 +6,7 @@ system('git clone git@github.com:pulseq/pulseq.git');
 addpath pulseq/matlab
 
 % get toolbox to convert .seq file to a PulSeg intermediate representation
-%system('git clone --branch dev git@github.com:HarmonizedMRI/pulseg.git');
-%addpath pulseg/matlab
-%addpath(genpath('pulseg/matlab/third_party'));
-addpath ~/github/HarmonizedMRI/pulseg/matlab/
-addpath(genpath('~/github/HarmonizedMRI/pulseg/matlab/third_party'));
+system('git clone --branch dev git@github.com:HarmonizedMRI/pulseg.git');
+addpath pulseg/matlab
+addpath(genpath('pulseg/matlab/third_party'));
+
